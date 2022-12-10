97fd3756aeefc33047aef137f0cb22736501f3d0
HarcheSamir <100028544+HarcheSamir@users.noreply.github.com>
2022-12-10 01:24:33 -0800

    Update property.jsx

--- pages/property.jsx
+++ pages/property.jsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import React from 'react';
-import propertyImg from '../public/assets/projects/bloodappcover.jpg';
+import propertyImg from '../public/assets/projects/bloodappcover.png';
 import { RiRadioButtonFill } from 'react-icons/ri';
 import Link from 'next/link';
 
