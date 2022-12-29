72d2f562857597fa4d8117b017d86591b72418d4
HarcheSamir <100028544+HarcheSamir@users.noreply.github.com>
2022-12-29 02:08:33 -0800

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -63,8 +63,8 @@ useEffect(()=> {
             className="rounded-full"
               src={me}
               alt='/'
-              width='70'
-              height='70'
+              width='60'
+              height='60'
              
             />
           </a>
